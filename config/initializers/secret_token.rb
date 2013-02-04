4b146a999b19a9fa732b6f8988bf813b167b57be
Dharam Gollapudi <dharam.gollapudi@gmail.com>
2013-02-03 17:53:41 -0800

    initial commit: new rails app without sprockets, test-unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Viralbeat::Application.config.secret_token = '7124214161c26f5d05aedaa81b80e00d2ae689c77033a201c4e2468f1c3968cf52ebacef50db250a324473829f74f640d0855b8abc70f9a894d104d0cf3d8927'
