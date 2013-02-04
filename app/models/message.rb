25342ba73baa8f4751e5cfe1b996e246fc51bce2
Dharam Gollapudi <dharam.gollapudi@gmail.com>
2013-02-03 20:27:59 -0800

    added validation check to the message model to pass the test case

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,9 @@
 class Message < ActiveRecord::Base
+  belongs_to :user
+  
   attr_accessible :title, :body
 
+  validates :user, :presence => true
   validates :title, :presence => true
   validates :body, :presence => true
 end
