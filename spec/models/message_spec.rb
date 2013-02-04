41fc0ce4949f83b1b1f794c0548bf72c211dc6f5
Dharam Gollapudi <dharam.gollapudi@gmail.com>
2013-02-03 20:21:26 -0800

    added another test case to the message spec

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe Message do
   it "is not valid without title" do
-    Message.create().should_not be_valid
+    Message.create(:title => nil, :body => "message").should_not be_valid
   end
+
+  it "is not valid without body" do
+    Message.create(:title => "test", :body => nil).should_not be_valid
+  end  
 end
