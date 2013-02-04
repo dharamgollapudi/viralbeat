6f0e567d93e761da13e96b86c2215bab88caf5f1
Dharam Gollapudi <dharam.gollapudi@gmail.com>
2013-02-04 00:11:59 -0800

    implemented the message creation using ajax feature within the messages list view

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,20 @@
 class MessagesController < ApplicationController
+  before_filter :authenticate_user!
 
   def index
     @messages = Message.all
   end
+
+  def new
+    @message = current_user.messages.build
+  end
+
+  def create
+    @message = current_user.messages.build(params[:message])
+
+    if @message.save
+      flash[:notice] = "Successfully created Message."
+      @messages = Message.all
+    end    
+  end  
 end
--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,27 +1,40 @@
 require 'spec_helper'
+include Warden::Test::Helpers
+Warden.test_mode!
+
+# Capybara.register_driver :selenium do |app|
+#   Capybara::Selenium::Driver.new(app, :browser => :chrome)
+# end
 
 describe 'messages controller' do
+  before(:each) do
+    User.destroy_all
+    @user = User.create(
+              :email => 'test@viralbeat.com',
+              :password => 'password',
+              :password_confirmation => 'password'
+            )
+    login_as @user, :scope => :user
+  end
+
+  after(:each) do
+    Warden.test_reset!
+  end
+
   describe 'index page' do
     it 'should have message list page selector' do
-      visit '/messages'
+      visit messages_path
       page.should have_selector('#message_list')
     end
 
     it 'should display no messages if there are no messages in the system' do
       Message.delete_all 
-      visit '/messages'
+      visit messages_path
       page.should have_content('No Messages')
     end
 
     it 'should have message id selection' do
-      User.destroy_all
-      Message.destroy_all      
-      user = User.create(
-        :email => 'test@viralbeat.com',
-        :password => 'password',
-        :password_confirmation => 'password'
-      )
-      message = user.messages.create(
+      message = @user.messages.create(
         :title => "test title",
         :body => "test body"
       )
@@ -30,12 +43,16 @@ describe 'messages controller' do
     end
 
     it 'lets the user post a message', :js => true do
+      Message.delete_all 
       visit messages_path
-      click_link "message_new"
-      fill_in 'Title', :with => 'Test Title'
-      fill_in 'Body', :with => 'Test Body'
+      page.should have_selector('#message_new')
+      click_button "message_new"
+      page.should have_selector('#message_form', :visible => true)
+      page.should have_content('Submit a Posting')
+      fill_in 'message[title]', :with => 'Test Title'
+      fill_in 'message[body]', :with => 'Test Body'
       click_on 'Submit'
       page.should have_content('Test Title')
-    end    
+    end 
   end
 end
